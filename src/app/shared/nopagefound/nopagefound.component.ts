9a36e8d9894bed561fbbcc1a1d26271f45bae81c
Adolfo  David Parra <adavidparra0412@gmail.com>
2020-07-03 13:25:08 -0500

    David

---
+++ src/app/shared/nopagefound/nopagefound.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-nopage-found',
+  templateUrl: './nopagefound.component.html',
+  styles: [
+  ]
+})
+export class NopageFoundComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
